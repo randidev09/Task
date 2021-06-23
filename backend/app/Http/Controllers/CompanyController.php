469581f1c79925990c21eed430d1b3407326a7b2
kali <kali@kali.kali>
2021-06-23 02:39:35 -0700

    Company API

---
+++ backend/app/Http/Controllers/CompanyController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Company;
+
+class CompanyController extends Controller
+{
+    public function index(){
+        return Company::all();
+    }
+
+    public function create(request $request){
+        $company            = new Company;
+        $company->comp_name = $request->comp_name;
+        $company->address   = $request->address;
+        $company->phone     = $request->phone;
+        $company->save();
+
+        return response()->json([
+            'code' => '200',
+            'message' => 'Successfully create company'
+        ]);
+    }
+
+    public function update(request $request, $id){
+        $comp_name  = $request->comp_name;
+        $address    = $request->address;
+        $phone      = $request->phone;
+
+        $company            = Company::find($id);
+        $company->comp_name = $comp_name;
+        $company->address   = $address;
+        $company->phone     = $phone;
+        $company->save();
+
+        return response()->json([
+            'code' => '200',
+            'message' => 'Successfully update company',
+            'company' => $company
+        ]);
+    }
+
+    public function delete($id){
+        $company = Company::find($id);
+        $company->delete();
+
+        return response()->json([
+            'code' => '200',
+            'message' => 'Successfully delete company'
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,22 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::get('user','App\Http\Controllers\UsersController@index');
 Route::post('user','App\Http\Controllers\UsersController@create');
 Route::put('/user/{id}','App\Http\Controllers\UsersController@update');
 Route::delete('/user/{id}','App\Http\Controllers\UsersController@delete');
+
+Route::get('company','App\Http\Controllers\CompanyController@index');
+Route::post('company','App\Http\Controllers\CompanyController@create');
+Route::put('/company/{id}','App\Http\Controllers\CompanyController@update');
+Route::delete('/company/{id}','App\Http\Controllers\CompanyController@delete');
